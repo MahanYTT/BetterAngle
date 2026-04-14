b8df9853f17c67b4265bedaa34fb9c4dc6220992
wavedropmaps <263283507+wavedropmaps@users.noreply.github.com>
2026-04-14 19:36:29 +1000

    v4.27.193: Clean Selection Overhaul with Robust Tagging

--- include/shared/State.h
+++ include/shared/State.h
@@ -20,7 +20,7 @@ std::wstring GetProfilesPath();
 #ifndef V_MAJ
 #define V_MAJ 4
 #define V_MIN 27
-#define V_PAT 192
+#define V_PAT 193
 #endif
 extern std::string g_lastVersionRun;
 extern bool g_setupComplete;
